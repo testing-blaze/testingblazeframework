1b3245ef076ca0f7b17c15c077507937250a52bc
Nauman Shahid <nauman.shahid@C02XT4NLJGH5.local>
2020-04-07 22:56:46 -0400

    udpated licensing

--- operations/src/main/java/com/testingblaze/register/I.java
+++ operations/src/main/java/com/testingblaze/register/I.java
@@ -1,9 +1,9 @@
 /*
  * Copyright 2020
  *
- * This file is part of  Testing Blaze Automation Framework [BDD] .
+ * This file is part of  Testing Blaze Automation Solution.
  *
- * Testing Blaze Automation Framework is licensed under the Apache License, Version
+ * Testing Blaze Automation Solution is licensed under the Apache License, Version
  * 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy
  * of the License at
