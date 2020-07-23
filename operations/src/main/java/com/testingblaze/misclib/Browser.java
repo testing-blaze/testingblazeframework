87ad2ca245f7796273bab9dbdc47fe2d69fac29c
John Phillips <john.phillips@reisystems.com>
2020-07-23 09:57:05 -0400

    Rearanging methods

--- operations/src/main/java/com/testingblaze/misclib/Browser.java
+++ operations/src/main/java/com/testingblaze/misclib/Browser.java
@@ -1,6 +1,5 @@
 package com.testingblaze.misclib;
 
-import com.testingblaze.actionsfactory.abstracts.Element;
 import com.testingblaze.actionsfactory.elementfunctions.JavaScript;
 import com.testingblaze.controller.DeviceBucket;
 import com.testingblaze.objects.InstanceRecording;
@@ -10,8 +9,8 @@ import org.openqa.selenium.WebDriver;
 
 public final class Browser {
 
-    private WebDriver driver;
-    private JavaScript javaScript;
+    private final WebDriver driver;
+    private final JavaScript javaScript;
 
     public Browser() {
         this.driver = InstanceRecording.getInstance(DeviceBucket.class).getDriver();
@@ -19,14 +18,13 @@ public final class Browser {
     }
 
     /**
-     * navigate to a specific url
+     * close the current tab
      *
-     * @param url
      * @author nauman.shahid
      */
-    public void navigateToUrl(String url) {
-        driver.navigate().to(url);
-        I.amPerforming().updatingOfReportWith().write(LogLevel.TEST_BLAZE_INFO, "Navigated to URL " + url);
+    public void closeBrowserOrTab() {
+        I.amPerforming().updatingOfReportWith().write(LogLevel.TEST_BLAZE_INFO, "Closed Tab " + driver.getTitle());
+        driver.close();
     }
 
     /**
@@ -50,6 +48,17 @@ public final class Browser {
     }
 
     /**
+     * navigate to a specific url
+     *
+     * @param url
+     * @author nauman.shahid
+     */
+    public void navigateToUrl(String url) {
+        driver.navigate().to(url);
+        I.amPerforming().updatingOfReportWith().write(LogLevel.TEST_BLAZE_INFO, "Navigated to URL " + url);
+    }
+
+    /**
      * refresh page
      *
      * @author nauman.shahid
@@ -60,20 +69,13 @@ public final class Browser {
     }
 
     /**
-     * close the current tab
+     * get the current url of web page
      *
      * @author nauman.shahid
      */
-    public void closeBrowserOrTab() {
-        I.amPerforming().updatingOfReportWith().write(LogLevel.TEST_BLAZE_INFO, "Closed Tab " + driver.getTitle());
-        driver.close();
-    }
-
-    /**
-     * @return status of the page load like Completed , loading , Interactive
-     */
-    public String getPageLoadStatus() {
-        return javaScript.getPageLoadStatus();
+    public String getCurrentUrl() {
+        I.amPerforming().updatingOfReportWith().write(LogLevel.TEST_BLAZE_INFO, "Current url is " + driver.getCurrentUrl());
+        return driver.getCurrentUrl();
     }
 
     /**
@@ -84,13 +86,10 @@ public final class Browser {
     }
 
     /**
-     * get title of current web page
-     *
-     * @author nauman.shahid
+     * @return status of the page load like Completed , loading , Interactive
      */
-    public String getPageTitle() {
-        I.amPerforming().updatingOfReportWith().write(LogLevel.TEST_BLAZE_INFO, "Fetched title " + driver.getTitle());
-        return driver.getTitle();
+    public String getPageLoadStatus() {
+        return javaScript.getPageLoadStatus();
     }
 
     /**
@@ -104,19 +103,19 @@ public final class Browser {
     }
 
     /**
-     * get the current url of web page
+     * get title of current web page
      *
      * @author nauman.shahid
      */
-    public String getCurrentURL() {
-        I.amPerforming().updatingOfReportWith().write(LogLevel.TEST_BLAZE_INFO, "Current url is " + driver.getCurrentUrl());
-        return driver.getCurrentUrl();
+    public String getPageTitle() {
+        I.amPerforming().updatingOfReportWith().write(LogLevel.TEST_BLAZE_INFO, "Fetched title " + driver.getTitle());
+        return driver.getTitle();
     }
 
     /**
      * Get current double value of current off set or movement of the page from original position
      *
-     * @return double y-axis
+     * @return double x-axis
      * @author nauman.shahid
      */
     public double getPageXOffSet() {
