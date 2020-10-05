000a4a91190a48f53dcec94204b753d768a77a13
nauman.shahid <Wakeup@761>
2020-10-04 20:02:03 -0400

    configured all settings for selg healing

---
+++ operations/src/main/java/com/testingblaze/controller/ByUsing.java
@@ -0,0 +1,15 @@
+package com.testingblaze.controller;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileSelector;
+
+public class Using extends MobileBy {
+    protected Using(MobileSelector selector, String locatorString) {
+        super(selector, locatorString);
+    }
+
+    public static String webXpath(){
+        
+    }
+}
+
